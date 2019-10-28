2478d5919986485adad58ac6d6317af238ee1de8
mutoe <mutoe@foxmail.com>
2019-10-28 19:29:02 +0800

    feat: intervene with rasa server

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ declare global {
   }
 }
 
+const uuid = +new Date()
+const rasaHost = 'http://localhost:5005'
+
 const ajContainer = createDom('div', { class: 'ask-jamie-container' })
 const bubbleContainer = createDom('div', { id: 'chat' })
 const chatButton = createDom('button', { class: 'chat-button', text: 'Ask Jamie' })
@@ -23,14 +26,39 @@ ajContainer.appendChild(bubbleContainer)
 document.body.appendChild(ajContainer)
 
 const chatWindow = new Bubbles(bubbleContainer, 'chatWindow', {
-   inputCallbackFn (o: any) {
-     console.log(o)
-    chatWindow.reply({says: ['test reply']})
+  inputCallbackFn (o: any) {
+    console.log('send', o)
+    chatWindow.think()
+    const xhr = new XMLHttpRequest()
+    xhr.open(`POST`, `${rasaHost}/webhooks/rest/webhook`, true)
+    xhr.setRequestHeader('content-type', 'application/json')
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4) {
+        if (xhr.getResponseHeader('content-type') === 'application/json') {
+          var result = JSON.parse(xhr.responseText)
+          console.log('result', result)
+          chatWindow.reply({
+            says: result.map((item: any) => {
+              if (item.image) return `<img src="${item.image}">`
+              return item.text
+            }),
+          })
+        } else {
+          console.log(xhr.responseText)
+        }
+      }
+
+    }
+    xhr.send(JSON.stringify({
+      'sender': uuid,
+      'message': o.input,
+    }))
+
   },
 })
 
 interface Convo {
-  [key: string] : {
+  [key: string]: {
     says?: string[]
     reply?: {
       question: string
